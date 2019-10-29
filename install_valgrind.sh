63fd4d2a36288d325eff6852dbf664c8dc65cde6
Youness Farini <farini.work@gmail.com>
2019-10-29 01:59:20 +0100

    Update install_valgrind.sh

--- install_valgrind.sh
+++ install_valgrind.sh
@@ -2,10 +2,10 @@ brew > brew_check 2>&1
 check=$(cat brew_check |  wc -l | tr -d " ")
 if[ $check -lt 5 ]
 then
-git clone https://github.com/Homebrew/brew ~/.brew
-echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc
-source $HOME/.zshrc
-brew help
+  git clone https://github.com/Homebrew/brew ~/.brew
+  echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc
+  source $HOME/.zshrc
+  brew help
 fi
 rm brew_check
 brew valgrind
