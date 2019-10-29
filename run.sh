38e9510888d2f22bdfc5786f9ea66e8f7d32c7cb
Youness Farini <farini.work@gmail.com>
2019-10-29 02:51:59 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ make cp_f
 gcc -Wall -Werror -Wextra compi_main.c gnl_cpy/*.c > cmp_error.txt 2>&1
 check=$(cat cmp_error.txt |  wc -l | tr -d " ")
 if [ $check -gt 0 ]; then
-    gcc -Wall -Werror -Wextra compi_main.c gnl_cpy/*.c
+    gcc -Wall -Werror -Wextra compi_main.c gnl_cpy/*.c -D BUFFER_SIZE=50
 else
     ./a.out
     rm ./a.out
