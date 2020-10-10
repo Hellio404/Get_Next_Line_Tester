c7063df60f51336ad8405ee9e843a08cb33daed2
ykoh <ykoh@student.42seoul.kr>
2020-10-10 20:44:29 +0900

    Fix bonus test

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # GET NEXT LINE TESTER
 1 - EDIT Makefile PATH\
 2 - bash run.sh
+3 - bash run_bonus.sh
---
+++ run_bonus.sh
@@ -0,0 +1,11 @@
+make cp_f_b
+gcc -Wall -Werror -Wextra compi_main.c gnl_cpy/*.c > cmp_error.txt -D BUFFER_SIZE=50 2>&1
+check=$(cat cmp_error.txt |  wc -l | tr -d " ")
+if [ $check -gt 0 ]; then
+    gcc -Wall -Werror -Wextra compi_main.c gnl_cpy/*.c -D BUFFER_SIZE=50
+else
+    ./a.out
+    rm ./a.out
+    make kk_b
+fi
+rm -f cmp_error.txt
